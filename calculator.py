9c833264c6f4525cb2395124c5ea8cdb6b205686
Prathna Saxena <prathna0299@gmail.com>
2019-04-04 20:08:30 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,17 @@
+class Calculator():
+	def add(a,b):
+		return a+b
+	def sub(a,b):
+		return a-b
+	def multiply(a,b):
+		return a*b
+	def divide(a,b):
+		return a/b
+x=Calculator.add(1,2)
+y=Calculator.sub(3,4)
+z=Calculator.multiply(5,6)
+p=Calculator.divide(7,8)
+print(x)
+print(y)
+print(z)
+print(p)
